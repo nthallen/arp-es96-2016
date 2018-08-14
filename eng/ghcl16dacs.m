35c0eab721eb18833d5a69d0283619ce001db472
Norton Allen <allen@huarp.harvard.edu>
2018-08-14 13:58:21 -0600

    Put PTRH data together in genui

--- eng/ghcl16dacs.m
+++ eng/ghcl16dacs.m
@@ -1,5 +1,5 @@
 function fig = ghcl16dacs(varargin);
 % ghcl16dacs(...)
 % DACS
-ffig = ne_group(varargin,'DACS','phcl16dacst','phcl16dacsp','phcl16dacsrh');
+ffig = ne_group(varargin,'DACS','phcl16dacst');
 if nargout > 0 fig = ffig; end
--- eng/ghcl16ptrh.m
+++ eng/ghcl16ptrh.m
@@ -1,5 +1,5 @@
 function fig = ghcl16ptrh(varargin);
 % ghcl16ptrh(...)
 % PTRH
-ffig = ne_group(varargin,'PTRH','phcl16ptrhs');
+ffig = ne_group(varargin,'PTRH','phcl16ptrht','phcl16ptrhp','phcl16ptrhrh','phcl16ptrhs');
 if nargout > 0 fig = ffig; end
